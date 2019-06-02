1470d9439bf2eeb9bfef0155ed9f619d431d2ba6
majki <michofilip@gmail.com>
2019-06-02 13:38:15 +0200

    Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
-import value.BooleanValue.BooleanConstant
-import value.{BooleanValue, ValueRepository}
+import value.{Value, ValueRepository}
 
 object Main extends App {
     
     implicit val repository: ValueRepository = new ValueRepository {}
-    val value1: BooleanValue = BooleanConstant(true)
-    println(value1.get)
+    val value1 = Value(true)
+    val value2 = Value(false)
+    println(value1 && value2 get)
     
 }
